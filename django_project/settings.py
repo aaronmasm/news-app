eb2a435d70bd39b2c08e92135aa84103d41f2399
aaronmasm <aaronmasm85@gmail.com>
2024-07-10 13:45:13 -0500

    static routes for build and build.sh

--- django_project/settings.py
+++ django_project/settings.py
@@ -134,16 +134,16 @@ STATIC_URL = "/static/"
 # This directory is used to store static files during development
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
+# This directory is used to store static files in production and for collectstatic command
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 if not DEBUG:
-    # In production, the static files will be collected into this directory
-    STATIC_ROOT = BASE_DIR / "staticfiles"
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 else:
-    # During development, we don't need to set STATIC_ROOT
-    STATIC_ROOT = None
     STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
